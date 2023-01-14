7e7c9f1df4e37661ee84a3c47ccfbfa90b4bec57
Kyle Orth <dakinko2456@gmail.com>
2023-01-14 12:36:08 -0700

    Database Update

--- main.py
+++ main.py
@@ -2,29 +2,49 @@ from data_collection import Collector
 from database_interactions import DatabaseInteractions
 
 
+DATABASE = "world_currency_history.db"
+TABLE = "exchange_rates"
+
+
 def add_dates(dates):
-    collect = Collector("world_currency_history.db", "exchange_rates")
-    collect.create_table()
+    collect = Collector(DATABASE, TABLE)
+    # collect.create_table()
     collect.collect_data(dates)
 
 
 def get_table():
-    db = DatabaseInteractions("world_currency_history.db")
-    print(db.select_multiple("exchange_rates", ["date", "USD", "JPY", "EUR"], "USD", descending=False))
+    db = DatabaseInteractions(DATABASE)
+    print(db.select_multiple(TABLE, ["date", "USD", "JPY", "EUR"], "date", descending=True))
+
+
+def delete_rows(dates):
+    db = DatabaseInteractions(DATABASE)
+    for date in dates:
+        criteria = "date ='{}'".format(date)
+        db.delete_row(TABLE, criteria, 'date', offset=1)
 
 
 if __name__ == "__main__":
+    newDay = False
+    requestData = False
+    deleteData = False
+    getData = True
+
     dates_list = []
     for i in range(12):
         month = str(i + 1)
         if len(month) == 1:
             month = "0{}".format(month)
-        dates_list.append("2014-{}-01".format(month))
+        dates_list.append("2016-{}-15".format(month))
 
-    requestData = False
+    if newDay:
+        Collector.zero_req_counter()
+
     if requestData:
         add_dates(dates_list)
 
-    getData = True
+    if deleteData:
+        delete_rows(dates_list)
+
     if getData:
         get_table()
